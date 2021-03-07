5da00d7cb08f34201ecfc3566b26b1245cfe7ae4
maharjanniru <nmaharjan@deerhold.org>
2021-03-07 19:20:18 +0545

    test

--- src/main/java/pages/DictionaryPageObject.java
+++ src/main/java/pages/DictionaryPageObject.java
@@ -2,22 +2,48 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class DictionaryPageObject {
     WebDriver driver;
 
+//    @FindBy(xpath="//*[@id=\"searchbar_input\"]")
+//    WebElement searchBar;
     By searchBar = By.xpath("//*[@id=\"searchbar_input\"]");
 
     By searchIcon = By.id("search-submit");
 
+    @FindBy(xpath="//*[@id=\"base-pw\"]/main/section/section/div[1]")
+    WebElement meaningSection;
+
+
     public DictionaryPageObject(WebDriver driver){
+
         this.driver =driver;
+        //This initElements method will create all WebElements
+        PageFactory.initElements(driver, this);
+
     }
 
     //set word
     public void setSearchWordInSearchBar(String word){
         driver.findElement(searchBar).sendKeys(word);
     }
+
+    public void clickSearchIcon(){
+        driver.findElement(searchIcon).click();
+    }
+
+    public void clearSearchBar(){
+       driver.findElement(searchBar).clear();
+    }
+
+    public String  getMeaning(){
+        return meaningSection.getText();
+    }
+
 
 
 
--- src/test/java/dictionary/TestDictionary.java
+++ src/test/java/dictionary/TestDictionary.java
@@ -7,8 +7,11 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import pages.DictionaryPageObject;
 
+import java.io.*;
+
 public class TestDictionary {
     String driverPath = "Drivers/chromedriver88.exe";
+    String filePath = "Words/words.txt";
     WebDriver driver;
     DictionaryPageObject dictionaryObj;
 
@@ -17,18 +20,35 @@ public class TestDictionary {
     public void setup(){
         System.setProperty("webdriver.chrome.driver", driverPath);
         ChromeOptions options = new ChromeOptions().setHeadless(false); // setHeadless(true) allows to run automation without displaying UI
-//        options.addArguments("start-maximized");
-//        options.addArguments("disable-infobars");
+        options.addArguments("start-maximized");
+        options.addArguments("disable-infobars");
       driver = new ChromeDriver(options);
         driver.get("https://www.dictionary.com/");
 
     }
 
     @Test(priority=0)
-    public void test(){
+    public void test() throws IOException, InterruptedException {
         //create DictionaryPageObject object
         dictionaryObj = new DictionaryPageObject(driver);
+        BufferedReader br = new BufferedReader(new FileReader(filePath));
+        StringBuilder allMeanings = new StringBuilder();
+        String line ="";
+        while((line = br.readLine() )!=null) {
+            System.out.println(line);
+            dictionaryObj.setSearchWordInSearchBar(line);
+            dictionaryObj.clickSearchIcon();
 
+            Thread.sleep(5000);
+            allMeanings.append( dictionaryObj.getMeaning());
+            allMeanings.append("\n");
+            dictionaryObj.clearSearchBar();
+
+        }
+        System.out.println(allMeanings);
+        FileWriter writer = new FileWriter("D:\\meanings.txt");
+        BufferedWriter buffer = new BufferedWriter(writer);
+        buffer.write(allMeanings.toString());
     }
 
 }
